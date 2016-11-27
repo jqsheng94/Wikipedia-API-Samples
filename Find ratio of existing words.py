8cbc6e1899fc5e19fa561abdf16c1baa0b1684e7
Jiaqi Sheng <jqsheng94@hotmail.com>
2016-11-27 14:23:31 -0800

    add file

---
+++ Find ratio of existing words.py
@@ -0,0 +1,24 @@
+from urllib.request import urlopen
+import simplejson as json
+import urllib.parse
+import re
+
+
+
+
+language = 'ar'
+Sentence ="""ليج اوف ليجيندز شرح القيم بلاي (الأساسيات)"""
+length = len(Sentence)
+Sentence = re.sub(r'[,|\(|\)|\[|\]\{\}|\#|\$|\%|\+|\&|\*|\^]', ' ', Sentence)
+WordsList = Sentence.split()
+WordsInWikipedia = 0
+for i in WordsList:
+    i = urllib.parse.quote(i)
+    link = json.load(urlopen("https://" + language + ".wikipedia.org/w/api.php?action=query&format=json&list=search&srsearch=" + i))
+    if 'continue' in link:
+        WordsInWikipedia += 1
+
+
+ratio = WordsInWikipedia/len(WordsList)
+print(ratio)
+
